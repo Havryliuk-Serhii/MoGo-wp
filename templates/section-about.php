b29270effc2cb18b80d48f86c93731aa0cd653a4
Serhii Havryliuk <48591061+Havryliuk-Serhii@users.noreply.github.com>
2020-03-19 22:57:51 +0200

    add all sections

--- templates/section-about.php
+++ templates/section-about.php
@@ -4,59 +4,59 @@
  */
  ?>
  <section  id="about">
-				<div class="container">
-					<div class="row">
-						<div class="col-md-12">
-							<h4 class="font-weight-normal text-center description">What we do</h4>
-							<h3 class="font-weight-bold text-center">Story about us</h3>
-							<div class="divider"></div>
-							<p class="text-center specification">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-						</div>
-					</div>
-				</div>
-				<div class="container">
-					<div class="row">
-						<div class="col-md-4 col-sm-12">
-							<div class="card mt-5 mb-5">
-								<img src="img/Rectangle1.png" alt="">
-							</div>
-						</div>
-						<div class="col-md-4 col-sm-12">
-							<div class="card mt-5 mb-5">
-								<img src="img/Rectangle2.png" alt="">
-							</div>
-						</div>
-						<div class="col-md-4 col-sm-12">
-							<div class="card mt-5 mb-5">
-								<img src="img/Rectangle3.png" alt="">
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="counter-section">
-					<div class="container">
-						<div class="row">
-							<div class="number text-center">
-								<h2 class="timer count-title count-number numeral-text font-weight-bold" data-to="42" data-speed="1500"></h2>
-								<span class="lead">Web design projects</span>
-							</div>
-							<div class="number text-center">
-								<h2 class="timer count-title count-number numeral-text font-weight-bold" data-to="123" data-speed="1500"></h2>
-								<span class="lead">Happy client</span>
-							</div>
-							<div class="number text-center">
-								<h2 class="timer count-title count-number numeral-text font-weight-bold" data-to="15" data-speed="1500"></h2>
-								<span class="lead">Award winner</span>
-							</div>
-							<div class="number text-center">
-								<h2 class="timer count-title count-number numeral-text font-weight-bold" data-to="99" data-speed="1500"></h2>
-								<span class="lead">Cup of coffee</span>
-							</div>
-							<div class="number text-center">
-								<h2 class="timer count-title count-number numeral-text font-weight-bold" data-to="24" data-speed="1500"></h2>
-								<span class="lead">Members</span>
-							</div>
-						</div>
-					</div>
-				</div>
-			</section>
+ 				<div class="container">
+ 					<div class="row">
+ 						<div class="col-md-12">
+ 							<h4 class="font-weight-normal text-center description-about">What we do</h4>
+ 							<h3 class="font-weight-bold text-center">Story about us</h3>
+ 							<div class="divider"></div>
+ 							<p class="text-center specification-about">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+ 						</div>
+ 					</div>
+ 				</div>
+ 				<div class="container">
+ 					<div class="row">
+ 						<div class="col-md-4 col-sm-12">
+ 							<div class="card mt-5 mb-5">
+ 								<img src="img/Rectangle1.png" alt="">
+ 							</div>
+ 						</div>
+ 						<div class="col-md-4 col-sm-12">
+ 							<div class="card mt-5 mb-5">
+ 								<img src="img/Rectangle2.png" alt="">
+ 							</div>
+ 						</div>
+ 						<div class="col-md-4 col-sm-12">
+ 							<div class="card mt-5 mb-5">
+ 								<img src="img/Rectangle3.png" alt="">
+ 							</div>
+ 						</div>
+ 					</div>
+ 				</div>
+ 			</section>
+ 			<section class="counter-section">
+ 				<div class="container">
+ 					<div class="row">
+ 						<div class="number text-center">
+ 							<h2 class="timer count-title count-number numeral-text font-weight-bold" data-to="42" data-speed="1500"></h2>
+ 							<span class="lead">Web design projects</span>
+ 						</div>
+ 						<div class="number text-center">
+ 							<h2 class="timer count-title count-number numeral-text font-weight-bold" data-to="123" data-speed="1500"></h2>
+ 							<span class="lead">Happy client</span>
+ 						</div>
+ 						<div class="number text-center">
+ 							<h2 class="timer count-title count-number numeral-text font-weight-bold" data-to="15" data-speed="1500"></h2>
+ 							<span class="lead">Award winner</span>
+ 						</div>
+ 						<div class="number text-center">
+ 							<h2 class="timer count-title count-number numeral-text font-weight-bold" data-to="99" data-speed="1500"></h2>
+ 							<span class="lead">Cup of coffee</span>
+ 						</div>
+ 						<div class="number text-center">
+ 							<h2 class="timer count-title count-number numeral-text font-weight-bold" data-to="24" data-speed="1500"></h2>
+ 							<span class="lead">Members</span>
+ 						</div>
+ 					</div>
+ 				</div>
+ 			</section> 
